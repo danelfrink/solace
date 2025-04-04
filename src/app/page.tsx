de0eaa52930cdc088c4fc8197063e792d303bc63
dan <danelfrink03@gmail.com>
2025-04-04 14:06:47 -0500

    use memo for filtering advocates and search term to reactive value

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,16 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Advocate } from "./api/advocates/advocate.type";
 
 
 export default function Home() {
-  const [advocates, setAdvocates] = useState([]);
-  const [filteredAdvocates, setFilteredAdvocates] = useState([]);
-
-  useEffect(() => {
-    console.log("fetching advocates...");
-    fetch("/api/advocates").then((response) => {
-      response.json().then((jsonResponse) => {
-        setAdvocates(jsonResponse.data);
-        setFilteredAdvocates(jsonResponse.data);
-      });
-    });
-  }, []);
-
-  const onChange = (e) => {
-    const searchTerm = e.target.value;
-
-    document.getElementById("search-term").innerHTML = searchTerm;
-
-    console.log("filtering advocates...");
-    const filteredAdvocates = advocates.filter((advocate: Advocate) => {
+  const [advocates, setAdvocates] = useState<Advocate[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  
+  const filterAdvocates = (advocates: Advocate[], searchTerm: string) => {
+    debugger;
+    return advocates.filter((advocate: Advocate) => {
       return (
         advocate.firstName.includes(searchTerm) ||
         advocate.lastName.includes(searchTerm) ||
@@ -34,13 +20,28 @@ export default function Home() {
         advocate.yearsOfExperience.toString().includes(searchTerm)
       );
     });
-
-    setFilteredAdvocates(filteredAdvocates);
   };
+  const filteredAdvocates = useMemo(() => filterAdvocates(advocates, searchTerm), [advocates, searchTerm])
+  
+
+  useEffect(() => {
+    console.log("fetching advocates...");
+    fetch("/api/advocates").then((response) => {
+      response.json().then((jsonResponse) => {
+        setAdvocates(jsonResponse.data);
+      });
+    });
+  }, []);
+
+
 
   const onClick = () => {
     console.log(advocates);
-    setFilteredAdvocates(advocates);
+    setSearchTerm("");
+  };
+
+  const handleSearchTermBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
   };
 
   return (
@@ -53,12 +54,12 @@ export default function Home() {
         <p>
           Searching for: <span id="search-term"></span>
         </p>
-        <input style={{ border: "1px solid black" }} onChange={onChange} />
+        <input style={{ border: "1px solid black" }} defaultValue={searchTerm} onChangeCapture={handleSearchTermBlur}/>
         <button onClick={onClick}>Reset Search</button>
       </div>
       <br />
       <br />
-      <table>
+      <table className="border-collapse table-auto">
         <thead>
           <tr>
             <th>First Name</th>
